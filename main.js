b7e06194144a598bee69e2fc2b7d957b9f118314
Michael Sankovich <misankovich@gmail.com>
2017-05-23 22:35:28 -0400

    initial

---
+++ main.js
@@ -0,0 +1,85 @@
+$(document).ready(() => {
+  const tdString = `
+    <tr class='data-row' id="{CONVENTION_ID}">
+      <td>{CONVENTION_NAME}</td>
+      <td>{CONVENTION_LOCATION}</td>
+      <td>{PARTICIPANT_LIST}</td>
+      <td>{CONVENTION_START_DATE} -- {CONVENTION_END_DATE}</td>
+      <td>
+        <button id="{CONVENTION_ID}_YES">YES</button>
+        <button id="{CONVENTION_ID}_NO">NO</button>
+      </td>
+    </tr>
+  `;
+
+  const processTdString = (values) => {
+    let tdStringCopy = tdString;
+
+    tdStringCopy = tdStringCopy
+      .replace(/{CONVENTION_ID}/g, `${values.convention_name}-${values.convention_start_date}-${values.convention_end_date}`)
+      .replace("{CONVENTION_NAME}", values.convention_name)
+      .replace("{CONVENTION_LOCATION}", values.convention_location)
+      .replace("{PARTICIPANT_LIST}", "Michael Sankovich")
+      .replace("{CONVENTION_START_DATE}", values.convention_start_date)
+      .replace("{CONVENTION_END_DATE}", values.convention_end_date);
+
+    $('#table_stuff tr:last').after(tdStringCopy);
+  }
+
+  [
+    {
+      convention_name: "first",
+      convention_start_date: "2017-07-03",
+      convention_end_date: "2017-07-08",
+      convention_location: "first place",
+    }, {
+      convention_name: "second",
+      convention_start_date: "2017-07-03",
+      convention_end_date: "2017-07-08",
+      convention_location: "second place",
+    }, {
+      convention_name: "third",
+      convention_start_date: "2017-07-03",
+      convention_end_date: "2017-07-08",
+      convention_location: "third place",
+    }, {
+      convention_name: "fourth",
+      convention_start_date: "2017-07-03",
+      convention_end_date: "2017-07-08",
+      convention_location: "fourth place",
+    }
+  ].forEach(e => processTdString(e));
+
+  $('#form_submit').submit((e) => {
+    e.preventDefault();
+
+    const values = {};
+
+    const $data = $('form_submit div:input');
+
+    const inputValues = [
+      document.getElementById('convention_name'),
+      document.getElementById('convention_location'),
+      document.getElementById('convention_start_date'),
+      document.getElementById('convention_end_date'),
+    ];
+    let errors = false;
+
+    inputValues.forEach((input) => {
+      if (input.id) {
+        if (!input.value || input.value === "") {
+          errors = true;
+        }
+        values[input.id] = input.value;
+      }
+    });
+
+    if (errors) {
+      alert("All Fields are Required");
+      return;
+    }
+
+    processTdString(values);
+  });
+
+});
